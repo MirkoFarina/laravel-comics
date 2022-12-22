dcb29f657dcdcd78d7c3ad9dab949667207b4f1b
MirkoFarina <MirkoFarina@outlook.com>
2022-12-22 16:46:40 +0100

    [add] card and footer

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -9,7 +9,118 @@
             <div class="container">
                 <h2 class="rectangle">CURRENT SERIES</h2>
                 <div class="row">
-
+                    <div class="card">
+                        <div class="top">
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                                alt="Action Comics" />
+                            <div class="content">
+                                <h5>PREZZO: $19.99</h5>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <h3>
+                                Action Comics
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                                alt="Action Comics" />
+                            <div class="content">
+                                <h5>PREZZO: $19.99</h5>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <h3>
+                                Action Comics
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                                alt="Action Comics" />
+                            <div class="content">
+                                <h5>PREZZO: $19.99</h5>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <h3>
+                                Action Comics
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                                alt="Action Comics" />
+                            <div class="content">
+                                <h5>PREZZO: $19.99</h5>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <h3>
+                                Action Comics
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                                alt="Action Comics" />
+                            <div class="content">
+                                <h5>PREZZO: $19.99</h5>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <h3>
+                                Action Comics
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                                alt="Action Comics" />
+                            <div class="content">
+                                <h5>PREZZO: $19.99</h5>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <h3>
+                                Action Comics
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                                alt="Action Comics" />
+                            <div class="content">
+                                <h5>PREZZO: $19.99</h5>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <h3>
+                                Action Comics
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="top">
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                                alt="Action Comics" />
+                            <div class="content">
+                                <h5>PREZZO: $19.99</h5>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <h3>
+                                Action Comics
+                            </h3>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="load-more">
@@ -18,5 +129,56 @@
                 </button>
             </div>
         </section>
+        <nav class="stylized-navbar">
+            <div class="container">
+                <ul>
+                    <li>
+                        <div class="img">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}"
+                            :alt="">
+                        </div>
+                        <a href="#">
+                            DIGITAL COMICS
+                        </a>
+                    </li>
+                    <li>
+                        <div class="img">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}"
+                            :alt="">
+                        </div>
+                        <a href="#">
+                            dc merchandise
+                        </a>
+                    </li>
+                    <li>
+                        <div class="img">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}"
+                            :alt="">
+                        </div>
+                        <a href="#">
+                            subscription
+                        </a>
+                    </li>
+                    <li>
+                        <div class="img">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}"
+                            :alt="">
+                        </div>
+                        <a href="#">
+                            comic shop locator
+                        </a>
+                    </li>
+                    <li>
+                        <div class="img">
+                            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}"
+                            :alt="">
+                        </div>
+                        <a href="#">
+                            dc power visa
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </main>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,165 @@
 <footer>
-    F
+    <div class="container">
+        <div class="col">
+            <nav>
+                <h4>
+                    dc comics
+                </h4>
+                <ul>
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                </ul>
+            </nav>
+            <nav>
+                <h4>
+                    shop
+                </h4>
+                <ul>
+                    <li>
+                        <a href="#">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col">
+            <nav>
+                <h4>
+                    dc
+                </h4>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Terms Of Use
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Privacy policy (New)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Ad Choices
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Advertising
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Jobs
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Subscriptions
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Talent Workshops
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            CPSC Certificates
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Ratings
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Shop Help
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Contact Us
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col">
+            <nav>
+                <h4>
+                    sites
+                </h4>
+                <ul>
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Power Visa</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="background-layer">
+        <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+    </div>
 </footer>
+<nav class="where-find-us">
+    <div class="container">
+        <div class="left">
+            <a href="#">sing-up now!</a>
+        </div>
+        <div class="right">
+            <span>
+                FOLLOW US
+            </span>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook">
+            </a>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter">
+            </a>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube">
+            </a>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest">
+            </a>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope">
+            </a>
+        </div>
+    </div>
+</nav>
